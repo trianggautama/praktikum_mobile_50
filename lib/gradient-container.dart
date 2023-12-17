9ab0210744ad134bb779dd92e142ffd8c460e6a3
trianggautama <trianggategarutama@gmail.com>
2023-12-17 21:44:45 +0800

    memisah class widget ke beberapa file terpisah

---
+++ lib/gradient-container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styledText.dart';
+
+class Gradientcontainer extends StatelessWidget {
+  const Gradientcontainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 51, 10, 107),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient-container.dart';
 
 // devine a function
 void main() {
 // execute functioon
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: Gradientcontainer(),
       ),
     ),
   );
-}
-
-class Gradientcontainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 51, 10, 107),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World !',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
